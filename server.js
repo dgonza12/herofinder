36b9d47fe59a2bb28b53f4f40ee7412cb5889f7b
Danny Gonzalez <gdanny120@gmail.com>
2018-01-06 15:05:28 -0800

    Trying socket IO

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 const express = require('express');
+const http = require('http');
 const app = express();
+const socketIO = require('socket.io');
+
+const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
+app.listen(port);
+
+
+/*socket Io */
+const server = http.createServer(app);
+
+const io = socketIO(server);
+
+
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+});
