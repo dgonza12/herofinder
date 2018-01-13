fcaf381f47293201a70db43e8067ed9e38f82cf9
Danny Gonzalez <gdanny120@gmail.com>
2018-01-12 16:59:13 -0800

    fixing error due to a missing component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
